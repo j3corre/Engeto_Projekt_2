ac5c941a71c49d7db5ff00e96c4655e7e213a8ed
Jan Blaha <jan.blaha@bcas.cz>
2025-07-31 13:47:00 +0200

    The polished and more general code.

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ email: jan.blaha@bcas.cz
 
 import random
 
+DIGIT_COUNT = 4         # Length of number to guess.
+DIVIDER = "-" * 47      # The divider line in listing.
+
 def make_game() -> list:
     """
     Prepares four digits to guess.
@@ -15,27 +18,27 @@ def make_game() -> list:
 
     game = list()
 
-    for index in range(4):
-        vytahl = random.choice(list(choices))
-        choices.discard(vytahl) # Digits can't repeat, therefore discard already chosen ones.
-        game.append(str(vytahl))
+    for index in range(DIGIT_COUNT):
+        chosen_digit = random.choice(list(choices))
+        choices.discard(chosen_digit) # Digits can't repeat, therefore discard already chosen ones.
+        game.append(str(chosen_digit))
         
-        if index == 0:          # Add digit 0 to chices for the next 3 digits.
+        if index == 0:          # Add digit 0 to choices for the next 3 digits.
             choices.add(0)
 
-    return(game)
+    return game
 
 def welcome_user():
     """
     Prints welcome message on the beginning of the game.
     """
     print(f"Hi there!")
-    print(f"-----------------------------------------------")
-    print(f"I've generated a random 4 digit number for you.")
+    print(DIVIDER)
+    print(f"I've generated a random {DIGIT_COUNT} digit number for you.")
     print(f"Let's play a bulls and cows game.")
-    print(f"-----------------------------------------------")
+    print(DIVIDER)
     print(f"Enter a number:")
-    print(f"-----------------------------------------------")
+    print(DIVIDER)
 
 def get_guess() -> str:
     """
@@ -45,27 +48,21 @@ def get_guess() -> str:
 
     while not(correct_guess):
         guess = input(">>>> ")
-        if len(guess) != 4:     # Guess has not allowed length.
-            print(f"Your guess has to be exactly 4 digits long. Try it again.")
-            print(f"-----------------------------------------------")
+        if len(guess) != DIGIT_COUNT:     # Guess has not allowed length.
+            print(f"Your guess has to be exactly {DIGIT_COUNT} digits long. Try it again.")
+            print(DIVIDER)
         elif guess[0] == "0":   # Guess starts with degit 0.
             print(f"Your guess can't start with digit 0. Try it again.")
-            print(f"-----------------------------------------------")
-        elif len([digit for digit in guess if digit.isdecimal()]) < 4:  # Guess has non digit characters.
+            print(DIVIDER)
+        elif len([digit for digit in guess if digit.isdecimal()]) < DIGIT_COUNT:  # Guess has non digit characters.
             print(f"Your guess has to have digit characters only. Try it again.")
-            print(f"-----------------------------------------------")
-        else:   # Check for all digits different.
-            digits = dict()
-            for char in guess:
-                if char in digits.keys():
-                    digits[char] += 1
-                else:
-                    digits[char] = 1
-            if len(digits) == 4:
-                correct_guess = True    # We get correct guess, return it.
-            else:
-                print(f"You guess contains some digit multiple times, that is not allowed. Try it again.")
-                print(f"-----------------------------------------------")
+            print(DIVIDER)
+        elif len(set(guess)) < DIGIT_COUNT:   # Check for all digits different.
+            print(f"You guess contains some digit multiple times, that is not allowed. Try it again.")
+            print(DIVIDER)
+        else: 
+            correct_guess = True    # We get correct guess, return it.
+                
     return guess
 
 def main():
@@ -73,26 +70,26 @@ def main():
     game = make_game()
 
     welcome_user()
-#    print(game)    # Uncomment out for debug purposes.
+#    print(game)    # Uncomment for debug purposes.
 
-    while bulls < 4:    # Play until user finds 4 bulls (all digits on its positions).
+    while bulls < DIGIT_COUNT:    # Play until user finds DIGIT_COUNT bulls (all digits on its positions).
         bulls, cows = 0, 0
         guess = get_guess()
         
-        for index in range(4):
+        for index in range(DIGIT_COUNT):
             if game[index] == guess[index]:
                 bulls += 1
-            elif set(guess[index]) <= set(game):
+            elif guess[index] in game:
                 cows += 1
 
         print(f"{guess}: bulls: {bulls}, cows: {cows}")
-        print(f"-----------------------------------------------")
+        print(DIVIDER)
                 
         guesses += 1
 
     print(f"Correct, you've guessed the right number")
     print(f"in {guesses} guesses!")
-    print(f"-----------------------------------------------")
+    print(DIVIDER)
     print(f"That's amazing!")
 
 main()
